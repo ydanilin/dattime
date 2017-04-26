09294d57406460fa7c25b9608885246062f8a56e
Yuretzz <yuvede@gmail.com>
2017-04-26 21:56:39 +0300

    Zaebenil. Protestil

---
+++ src/epoch/epocher.py
@@ -0,0 +1,164 @@
+# coding=utf-8
+from datetime import datetime, timedelta
+from time import gmtime
+from calendar import timegm
+
+
+class Epocher:
+    """
+    domain dictionary:
+    base: all variables prefixed with "base" are the start of our
+          new Sfirat ha-Nocrim
+    ep:   prefix "ep" means our New Epoch
+    w:    prefix "w" means world regular time counting
+    time: all variables containing "time" always expressed in seconds
+    """
+    def __init__(self):
+        self.baseYear = 1976
+        self.baseMonth = 7
+        self.baseDay = 14
+        self.baseHour = 11  # in UTC
+        self.baseMinute = 30
+        self.wBaseTime = 0  # in world seconds
+        self.newSecondRatio = 0.864
+        self.ratioMinutes = 10
+        self.ratioHours = self.ratioMinutes * 100
+        self.ratioDays = self.ratioHours * 100
+        self.ratioMonths = self.ratioDays * 100
+        self.ratioYears = self.ratioMonths * 10
+
+    def adjustBaseTime(self, timezone):
+        h = int(timezone)
+        m = int(60*(timezone - h))
+        delta = timedelta(hours=h, minutes=m)
+        dt = datetime(self.baseYear,
+                      self.baseMonth,
+                      self.baseDay,
+                      self.baseHour,
+                      self.baseMinute) + delta
+        self.wBaseTime = timegm(dt.timetuple())
+
+    def todayWorldTime(self, timezone):
+        h = int(timezone)
+        m = int(60 * (timezone - h))
+        delta = timedelta(hours=h, minutes=m)
+        dt = datetime.utcnow() + delta
+        return dt
+
+    def worldTimeToEpochSeconds(self, wDt):
+        worldSeconds = timegm(wDt.timetuple())
+        epSeconds = (worldSeconds - self.wBaseTime) / self.newSecondRatio
+        return round(epSeconds, 0)
+
+    def epochSecondsToWorldTime(self, epSeconds):
+        worldSeconds = epSeconds*self.newSecondRatio + self.wBaseTime
+        dt = datetime.utcfromtimestamp(worldSeconds)
+        return dt
+
+    def epochSecondsToEpochTime(self, epSeconds):
+        direction = 'FST'
+        if epSeconds < 0:
+            direction = 'BST'
+        epYear = int(epSeconds / self.ratioYears)
+        t = epSeconds - epYear * self.ratioYears
+        epMonth = int(t / self.ratioMonths)
+        t = t - epMonth * self.ratioMonths
+        epDay = int(t / self.ratioDays)
+        t = t - epDay * self.ratioDays
+        epHour = int(t / self.ratioHours)
+        t = t - epHour * self.ratioHours
+        epMinute = int(t / self.ratioMinutes)
+        t = t - epMinute * self.ratioMinutes
+        epSecond = int(round(t))
+        return {'Year': epYear, 'Month': epMonth, 'Day': epDay,
+                'Hour': epHour, 'Minute': epMinute, 'Second': epSecond,
+                'Direction': direction}
+
+    def epochTimeToEpochSeconds(self, epTime):
+        t = epTime['Second'] + \
+            epTime['Minute'] * self.ratioMinutes + \
+            epTime['Hour'] * self.ratioHours + \
+            epTime['Day'] * self.ratioDays + \
+            epTime['Month'] * self.ratioMonths + \
+            epTime['Year'] * self.ratioYears
+        return t
+
+    def dropYearAndReverseToPositive(self, epTime):
+        ept = epTime
+        ept['Year'] = 0
+        seconds = self.epochTimeToEpochSeconds(ept)
+        seconds += 100000000
+        return self.epochSecondsToEpochTime(seconds)
+
+    def epNextDobSecondsAndEpNowSeconds(self, timezone, year, month, day, hour, minu, sec=0):
+        # calculate epMomentSeconds and epDobSeconds
+        self.adjustBaseTime(timezone)
+        wdtMoment = self.todayWorldTime(timezone)
+        epMomentSeconds = self.worldTimeToEpochSeconds(wdtMoment)
+        epMomentTime = self.epochSecondsToEpochTime(epMomentSeconds)
+        wdtDob = datetime(year, month, day, hour, minu, sec)
+        epDobSeconds = self.worldTimeToEpochSeconds(wdtDob)
+        # find NEXT epDobSeconds
+        epDobTime = self.epochSecondsToEpochTime(epDobSeconds)
+        if epDobTime[
+            'Direction'] == 'BST':  # check if DobTime before Start and reverse
+            epDobTime = self.dropYearAndReverseToPositive(epDobTime)
+        epDobTime['Year'] = epMomentTime['Year']
+        epDobSeconds = self.epochTimeToEpochSeconds(epDobTime)
+        if epDobSeconds < epMomentSeconds:  # if this year Dob past already
+            epDobSeconds += 100000000  # move forward to next year
+        return epDobSeconds, epMomentSeconds
+
+    # **********************************************************
+    #        interface functions
+    # **********************************************************
+    def getEpochTime(self, timezone):
+        self.adjustBaseTime(timezone)
+        wDt = self.todayWorldTime(timezone)
+        epSeconds = self.worldTimeToEpochSeconds(wDt)
+        return self.epochSecondsToEpochTime(epSeconds)
+
+    def getEpochDob(self, timezone, year, month, day, hour, minu, sec=0):
+        self.adjustBaseTime(timezone)
+        wDt = datetime(year, month, day, hour, minu, sec)
+        epSeconds = self.worldTimeToEpochSeconds(wDt)
+        return self.epochSecondsToEpochTime(epSeconds)
+
+    def getEpochAge(self, timezone, year, month, day, hour, minu, sec=0):
+        self.adjustBaseTime(timezone)
+        wdtMoment = self.todayWorldTime(timezone)
+        wdtDob = datetime(year, month, day, hour, minu, sec)
+        epMomentSeconds = self.worldTimeToEpochSeconds(wdtMoment)
+        epDobSeconds = self.worldTimeToEpochSeconds(wdtDob)
+        epAge = epMomentSeconds - epDobSeconds
+        return self.epochSecondsToEpochTime(epAge)
+
+    def getEpochNextDobDate(self, timezone, year, month, day, hour, minu, sec=0):
+        epDobSeconds = self.epNextDobSecondsAndEpNowSeconds(timezone, year, month, day, hour, minu, sec)[0]
+        return self.epochSecondsToEpochTime(epDobSeconds)
+
+    def getEpochNextDobWorldDate(self, timezone, year, month, day, hour, minu, sec=0):
+        epDobSeconds = self.epNextDobSecondsAndEpNowSeconds(timezone, year, month, day, hour, minu, sec)[0]
+        return self.epochSecondsToWorldTime(epDobSeconds)
+
+    def epochDaysToNextDob(self, timezone, year, month, day, hour, minu, sec=0):
+        epDobSeconds, epMomentSeconds = self.epNextDobSecondsAndEpNowSeconds(timezone, year, month, day, hour, minu, sec)
+        diff = epDobSeconds - epMomentSeconds
+        return self.epochSecondsToEpochTime(diff)
+
+if __name__ == '__main__':
+    ep = Epocher()
+    ep.adjustBaseTime(0)
+    print(datetime.utcfromtimestamp(ep.wBaseTime))
+    print(ep.todayWorldTime(0))
+    epSec = ep.worldTimeToEpochSeconds(datetime(1972, 7, 14, 14, 30))
+    print(epSec)
+    epT = ep.epochSecondsToEpochTime(epSec)
+    print(epT)
+    print(ep.epochTimeToEpochSeconds(epT))
+    print('Epoch Age', ep.getEpochAge(0, 1975, 11, 10, 12, 50))
+    print('Epoch Dob:', ep.getEpochDob(0, 1975, 11, 10, 12, 50))
+    print('Today epoch date:', ep.getEpochTime(0))
+    print('Epoch Next Dob', ep.getEpochNextDobDate(0, 1975, 11, 10, 12, 50))
+    print('World Next Dob', ep.getEpochNextDobWorldDate(0, 1975, 11, 10, 12, 50))
+    print('Epoch days to next Dob', ep.epochDaysToNextDob(0, 1975, 11, 10, 12, 50))
---
+++ src/tests/test_epocher.py
@@ -0,0 +1,179 @@
+import unittest
+from datetime import datetime
+from ..epoch import Epocher
+
+class Epocher_Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.ep = Epocher()
+
+    def test_adjustBaseTime(self):
+        # zero
+        self.ep.adjustBaseTime(0)
+        dat = datetime.utcfromtimestamp(self.ep.wBaseTime)
+        dtext = dat.isoformat()
+        self.assertEqual(dtext, '1976-07-14T11:30:00')
+        # plus 1
+        self.ep.adjustBaseTime(1)
+        dat = datetime.utcfromtimestamp(self.ep.wBaseTime)
+        dtext = dat.isoformat()
+        self.assertEqual(dtext, '1976-07-14T12:30:00')
+        # plus 1.5
+        self.ep.adjustBaseTime(1.5)
+        dat = datetime.utcfromtimestamp(self.ep.wBaseTime)
+        dtext = dat.isoformat()
+        self.assertEqual(dtext, '1976-07-14T13:00:00')
+        # minus 1
+        self.ep.adjustBaseTime(-1)
+        dat = datetime.utcfromtimestamp(self.ep.wBaseTime)
+        dtext = dat.isoformat()
+        self.assertEqual(dtext, '1976-07-14T10:30:00')
+        # minus 1.5
+        self.ep.adjustBaseTime(-1.5)
+        dat = datetime.utcfromtimestamp(self.ep.wBaseTime)
+        dtext = dat.isoformat()
+        self.assertEqual(dtext, '1976-07-14T10:00:00')
+
+    def test_worldTimeToEpochSecondsTZ2(self):
+        self.ep.adjustBaseTime(2)
+        # check against base time
+        wdt = datetime(1976, 7, 14, 13, 30)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, 0)
+        # check against date before Christ))
+        wdt = datetime(1975, 11, 10, 14, 50)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, -24694444)
+        # check against example in the requirements (1 494 500 000)
+        wdt = datetime(2017, 6, 14, 13, 30)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, 1494500000)
+        # get the duration of one new year
+        wdt = datetime(1979, 4, 10, 13, 30)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, 100000000)
+
+    def test_worldTimeToEpochSecondsTZ0(self):
+        self.ep.adjustBaseTime(0)
+        # check against base time
+        wdt = datetime(1976, 7, 14, 11, 30)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, 0)
+        # check against date before Christ))
+        wdt = datetime(1975, 11, 10, 12, 50)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, -24694444)
+        # check against example in the requirements (1 494 500 000)
+        wdt = datetime(2017, 6, 14, 11, 30)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, 1494500000)
+        # get the duration of one new year
+        wdt = datetime(1979, 4, 10, 11, 30)
+        es = self.ep.worldTimeToEpochSeconds(wdt)
+        self.assertEqual(es, 100000000)
+
+    def test_epochSecondsToWorldTime(self):
+        self.ep.adjustBaseTime(0)
+        dt = self.ep.epochSecondsToWorldTime(0)
+        self.assertEqual(dt.isoformat(), '1976-07-14T11:30:00')
+        dt = self.ep.epochSecondsToWorldTime(-24694444)
+        self.assertEqual(dt.isoformat(timespec='seconds'), '1975-11-10T12:50:00')
+        dt = self.ep.epochSecondsToWorldTime(1494500000)
+        self.assertEqual(dt.isoformat(), '2017-06-14T11:30:00')
+        dt = self.ep.epochSecondsToWorldTime(100000000)
+        self.assertEqual(dt.isoformat(), '1979-04-10T11:30:00')
+
+    def test_epochSecondsToEpochTime(self):
+        self.ep.adjustBaseTime(0)
+        output = self.ep.epochSecondsToEpochTime(1494500000)
+        self.assertEqual(output, {'Year': 14, 'Month': 9, 'Day': 45,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'FST'})
+        output = self.ep.epochSecondsToEpochTime(-1494500000)
+        self.assertEqual(output, {'Year': -14, 'Month': -9, 'Day': -45,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'BST'})
+        output = self.ep.epochSecondsToEpochTime(0)
+        self.assertEqual(output, {'Year': 0, 'Month': 0, 'Day': 0,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'FST'})
+
+    def test_epochTimeToEpochSeconds(self):
+        output = self.ep.epochTimeToEpochSeconds({'Year': -14, 'Month': -9,
+                                                  'Day': -45, 'Hour': 0,
+                                                  'Minute': 0, 'Second': 0,
+                                                  'Direction': 'BST'})
+        self.assertEqual(output, -1494500000)
+        output = self.ep.epochTimeToEpochSeconds({'Year': 0, 'Month': 0,
+                                                  'Day': -31, 'Hour': 0,
+                                                  'Minute': 0, 'Second': 0,
+                                                  'Direction': 'BST'})
+        self.assertEqual(output, -3100000)
+
+    def test_dropYearAndReverseToPositive(self):
+        self.ep.adjustBaseTime(0)
+        # this will be one month before Christ, so add year yelds month +9
+        wdt = datetime(1976, 4, 5, 11, 30)
+        epSeconds = self.ep.worldTimeToEpochSeconds(wdt)
+        epTime = self.ep.epochSecondsToEpochTime(epSeconds)
+        output = self.ep.dropYearAndReverseToPositive(epTime)
+        self.assertEqual(output, {'Year': 0, 'Month': 9, 'Day': 0,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'FST'})
+        # this demonstrates how it drops the year
+        wdt = datetime(1973, 7, 10, 11, 30)  # -1 year -1 month
+        epSeconds = self.ep.worldTimeToEpochSeconds(wdt)
+        epTime = self.ep.epochSecondsToEpochTime(epSeconds)
+        output = self.ep.dropYearAndReverseToPositive(epTime)
+        self.assertEqual(output, {'Year': 0, 'Month': 9, 'Day': 0,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'FST'})
+
+    def test_epNextDobSecondsAndEpNowSeconds(self):
+        t = self.ep.epNextDobSecondsAndEpNowSeconds(0, 1975, 11, 10, 11, 30)
+        wdt = self.ep.epochSecondsToWorldTime(t[0]).isoformat()
+        # my next stupid birthday will be in 2019
+        self.assertEqual(wdt, '2019-08-31T11:30:00')
+
+    def test_getEpochDob_tz0(self):
+        output = self.ep.getEpochDob(0, 1975, 11, 10, 11, 30)
+        self.assertEqual(output, {'Year': 0, 'Month': -2, 'Day': -47,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'BST'})
+
+    def test_getEpochDob_tz2(self):
+        output = self.ep.getEpochDob(2, 1975, 11, 10, 13, 30)
+        self.assertEqual(output, {'Year': 0, 'Month': -2, 'Day': -47,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'BST'})
+
+    def test_getEpochDob_tz_minus1d5(self):
+        output = self.ep.getEpochDob(-1.5, 1975, 11, 10, 10, 00)
+        self.assertEqual(output, {'Year': 0, 'Month': -2, 'Day': -47,
+                                  'Hour': 0, 'Minute': 0, 'Second': 0,
+                                  'Direction': 'BST'})
+
+    def test_getEpochNextDobDate_tz0(self):
+        output = self.ep.getEpochNextDobDate(0, 1975, 11, 10, 11, 30)
+        wdt = datetime(2019, 8, 31, 11, 30)
+        seconds = self.ep.worldTimeToEpochSeconds(wdt)
+        testt = self.ep.epochSecondsToEpochTime(seconds)
+        self.assertEqual(output, testt)
+
+    def test_getEpochNextDobDate_tz2(self):
+        output = self.ep.getEpochNextDobDate(2, 1975, 11, 10, 13, 30)
+        wdt = datetime(2019, 8, 31, 13, 30)
+        seconds = self.ep.worldTimeToEpochSeconds(wdt)
+        testt = self.ep.epochSecondsToEpochTime(seconds)
+        self.assertEqual(output, testt)
+
+    def test_getEpochNextDobDate_tz_minus1d5(self):
+        output = self.ep.getEpochNextDobDate(-1.5, 1975, 11, 10, 10, 00)
+        wdt = datetime(2019, 8, 31, 10, 00)
+        seconds = self.ep.worldTimeToEpochSeconds(wdt)
+        testt = self.ep.epochSecondsToEpochTime(seconds)
+        self.assertEqual(output, testt)
+
+
+if __name__ == '__main__':
+    unittest.main()
