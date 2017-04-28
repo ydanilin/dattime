f2d842f3d20fc39389d3e9fd3ca116ca8ad22926
Yuretzz <yuvede@gmail.com>
2017-04-28 11:35:27 +0300

    django forms started!

--- dattimewww/epoch/views.py
+++ dattimewww/epoch/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 from .models import Epocher
+
+
+class Form_inscription(forms.Form):
+    day = forms.CharField(label="Day", max_length=30)
+    month = forms.ChoiceField(label='Month', choices=[(1, 'Jan'), (2, 'Feb')])
+
 
 Ep = Epocher()
 
@@ -9,7 +16,7 @@ menuSet = [{'caption': 'Homepage', 'viewName': 'epochTime'},
            {'caption': 'The Bridge between calendars', 'viewName': 'bridge'},
            {'caption': 'The New Time', 'viewName': 'calculation'}]
 
-# Create your views here.
+
 def epochTime(request):
     # return HttpResponse('HUJ !!')
     footerItems = [menuSet[1], menuSet[2], menuSet[3]]
@@ -17,9 +24,22 @@ def epochTime(request):
     return render(request, 'epoch/screen01.html', {'epTime': epTime,
                                                    'footerItems': footerItems})
 
+
 def birthday(request):
     footerItems = [menuSet[0], menuSet[2], menuSet[3]]
-    return render(request, 'epoch/birthday.html', {'footerItems': footerItems})
+    if request.POST:
+        form = Form_inscription(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            return HttpResponse("Developer added")
+        else:
+            return render(request, 'epoch/birthday.html',
+                          {'footerItems': footerItems, 'form' : form})
+    else:
+        form = Form_inscription()
+        return render(request, 'epoch/birthday.html',
+                      {'footerItems': footerItems, 'form': form})
+
 
 def bridge(request):
     footerItems = [menuSet[0], menuSet[1], menuSet[3]]
